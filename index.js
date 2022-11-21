a7df73dade768e8e255e8f0d082415c62b574bb4
icika-max <icika-max>
2022-11-21 15:26:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,9 +11,23 @@ function shout(string) {
   function logWhisper(string) {  
     console.log(string.toLowerCase())                                                                                                
   }
-  function sayHiToHeadphonedRoommate(string) {
-    string== string.toLowerCase()
-    if(string= string.toLowerCase ()){
-    return 'I can\'t hear you!';}       
+  function sayHiToHeadphonedRoommate(string) 
+  {
+    if (string == string.toLowerCase ()){
+    return 'I can\'t hear you!';
+  }       
+  else if( string == string.toUpperCase()){
+    return 'YES INDEED!';
   }
-
+  
+  
+  else if( string == "Let\'s have dinner together!")
+  {
+    return 'I would love to!'
+  }
+}
+  // function sayHiToHeadphonedRoommate(string) {
+  //   string== string.toUpperCase()
+  //   if(string==string.toUpperCase ()){
+  //   return 'YES INDEED!';} 
+  // }
